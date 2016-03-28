218f4483e64f6060f95bf716b755e79f4dea9ccb
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-03-28 21:27:51 +0900

    modify comment in LazuriteGraph installer

--- LazuriteGraph/install.sh
+++ LazuriteGraph/install.sh
@@ -5,56 +5,56 @@ echo '***** Installing LazuriteGraph for Raspberry Pi ******'
 echo '******************************************************'
 echo ''
 echo ''
-echo 'get LazDriver from github'
-cd ~
-cd driver
+echo 'STEP1:: get LazDriver from github'
+
+cd ~/driver
 git clone git://github.com/LAPIS-Lazurite/LazDriver
 cd LazDriver
 
 echo ''
 echo ''
-echo 'build LazDriver'
+echo 'STEP2:: build LazDriver'
 make
 echo ''
 echo ''
-echo 'installing rxtx serial library'
+echo 'STEP3:: installing rxtx serial library'
 sudo apt-get install librxtx-java
 
 echo ''
 echo ''
-echo 'get LazuriteJava'
+echo 'STEP4:: get LazuriteJava'
 cd ~
 mkdir java
-cd java
 
-echo ''
-echo ''
-echo 'build LazuriteJava'
+cd java
 git clone git://github.com/LAPIS-Lazurite/LazuriteJava
 cd LazuriteJava
+echo ''
+echo ''
+echo 'STEP5:: build LazuriteJava'
 make
 echo ''
 echo ''
-echo 'Install LazuriteJava'
+echo 'STEP6:: Install LazuriteJava'
 make install
 echo ''
 echo ''
-echo 'get LazuriteGraph'
+echo 'STEP7:: get LazuriteGraph'
 cd ~/java
 git clone git://github.com/LAPIS-Lazurite/LazuriteGraph
 cd LazuriteGraph
 echo ''
 echo ''
-echo 'install external library'
+echo 'STEP8:: install external library'
 make install
 echo ''
 echo ''
-echo 'build LazuriteGraph'
+echo 'STEP9:: build LazuriteGraph'
 make
 echo ''
-echo 'enabling SubGHz tab'
+echo 'STEP10:: enabling SubGHz tab'
 cp lib/linux/graph.pref .
 echo ''
 echo ''
-echo 'End of install'
+echo 'Complete !!'
 
