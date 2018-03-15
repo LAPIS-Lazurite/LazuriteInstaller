026c72d7c2998093625526347651044096980593
tanaka-lapis-jp <tanaka802@dsn.lapis-semi.com>
2018-03-15 14:52:22 +0900

    add rpi-source option '-g' to fix kernel build error

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y bc doxygen npm node-gyp
 git clone https://github.com/notro/rpi-source
 cd rpi-source
 chmod +x rpi-source
-./rpi-source --skip-gcc
+./rpi-source -g --skip-gcc
 cd /home/pi/linux/arch/arm/boot/dts
 echo $WORK
 $WORK/LazCnv.awk bp3596_spi
