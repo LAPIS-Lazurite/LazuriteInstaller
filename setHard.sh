eb8e9cc1df2d2ba42dce31dbe700cd88610f4b74
Yoshiyuki Tanaka <tanaka802@dsn.lapis-semi.com>
2019-11-28 09:03:20 +0900

    change method to enable SPI/I2C

--- setHard.sh
+++ setHard.sh
@@ -3,26 +3,5 @@ WORK=$(pwd)
 sudo cp /boot/config.txt /boot/config.bak
 
 # enabling I2C & SPI
-awk '{if (/i2c_arm|spi/){if(substr($0,0,2)=="#"){print substr($0,2,length($0));}}else{print $0}}' /boot/config.bak > /boot/config.txt
-
-I2CDEV=`sudo cat /etc/modules |grep i2c-dev`
-
-if [ "$I2CDEV" = "i2c-dev" ] ; then
-	exit 0
-fi
-if [[ "$I2CDEV" = *"#"* ]] ; then
-	echo "remove #"
-	cp /etc/modules /etc/modules.bak
-	awk '{
-		if (/i2c-dev/) {
-			if (substr($0,0,2)=="#"){
-				print substr($0,2,length($0));
-			} else { print $0 }
-		} else {
-			print $0;
-		}
-	} ' /etc/modules.bak > /etc/modules
-else 
-	echo "i2c-dev" >> /etc/modules
-fi
-
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_i2c 0
